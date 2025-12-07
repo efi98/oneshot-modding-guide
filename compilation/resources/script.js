2bdf2668d164994d2f74f0be4371232c9a9e5d64
Efi <119013869+efi98@users.noreply.github.com>
2025-12-07 15:34:27 +0200

    Enhance theme toggle functionality and improve code organization

--- compilation/resources/script.js
+++ compilation/resources/script.js
@@ -1,19 +1,33 @@
-
 function copyCodeBlock(caller) {
-	const content = caller.target.parentNode.querySelector("code").innerText.replace(/\u00a0/g, " ");
-	navigator.clipboard.writeText(content)
+    const content = caller.target.parentNode.querySelector("code").innerText.replace(/\u00a0/g, " ");
+    navigator.clipboard.writeText(content)
+}
+
+function toggleTheme(theme = null) {
+    if (theme !== 'dark') {
+        document.body.classList.toggle('light');
+    }
+    const icon = document.getElementById('theme-icon');
+    if (document.body.classList.contains('light')) {
+        setTheme('light');
+        icon.textContent = '☀️';
+    } else {
+        setTheme('dark');
+        icon.textContent = '🌙';
+    }
+}
+
+function loadTheme() {
+    let theme = localStorage.getItem('theme');
+    return theme ?? '';
+}
+
+function setTheme(theme) {
+    localStorage.setItem('theme', theme);
 }
 
 window.onload = () => {
-	hljs.highlightAll()
-}
-
-function toggleTheme() {
-    document.body.classList.toggle('light');
-    const icon = document.getElementById('theme-icon');
-    if (document.body.classList.contains('light')) {
-        icon.textContent = '☀️';
-    } else {
-        icon.textContent = '🌙';
-    }
+    hljs.highlightAll();
+    const theme = loadTheme();
+    toggleTheme(theme);
 }
